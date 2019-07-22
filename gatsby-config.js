e1287cfc0f7780ebefa86cbaa9afc804f23a73fe
Ivan Stefanov <ivan.stefanov@clouway.com>
2019-07-22 16:39:49 +0300

    ga: integrate gatsby-plugin-google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,36 +14,36 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages`,
         name: 'pages',
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
     {
-      resolve: `gatsby-transformer-json`,
+      resolve: 'gatsby-transformer-json',
       options: {
-        typeName: `MenuItems`, // a fixed string
+        typeName: 'MenuItems', // a fixed string
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `menuItems`,
+        name: 'menuItems',
         path: `${__dirname}/src/menuItems`,
       },
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'docs.clouway.com',
         short_name: 'clouway docs',
@@ -55,19 +55,19 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           'gatsby-remark-component',
-          `gatsby-remark-katex`,
+          'gatsby-remark-katex',
           {
-            resolve: `gatsby-remark-autolink-headers`,
+            resolve: 'gatsby-remark-autolink-headers',
             options: {
               className: 'post-toc-anchor',
             },
           },
           {
-            resolve: `gatsby-remark-images`,
+            resolve: 'gatsby-remark-images',
             options: {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
@@ -83,7 +83,16 @@ module.exports = {
         ],
       },
     },
-    `gatsby-plugin-remove-trailing-slashes`
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-85799539-4',
+        head: false,
+        anonymize: true,
+        respectDNT: true
+      }
+    },
+    'gatsby-plugin-remove-trailing-slashes'
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
